88707c835859e4a9bf2637e8454b5a2411fcc81b
Kanisha Shah <kanishapshah@gmail.com>
2019-11-14 15:01:31 -0800

    made changes to RecipeStore

--- Frydge/Frydge/RecipeStore.swift
+++ Frydge/Frydge/RecipeStore.swift
@@ -9,13 +9,13 @@
 import Foundation
 
 class RecipeStore{
-    var recipeList: [Recipe] = [] // List of Recipes
+    static var recipeList: [Recipe] = [] // List of Recipes
     
-    public func add(addRecipe : Recipe){
+    static public func add(addRecipe : Recipe){
         recipeList.append(addRecipe)
     }
     
-    public func delete(delRecipe : Recipe){
+    static public func delete(delRecipe : Recipe){
         for (index, element) in recipeList.enumerated(){
             if element.id == delRecipe.id{
                 recipeList.remove(at: index)
