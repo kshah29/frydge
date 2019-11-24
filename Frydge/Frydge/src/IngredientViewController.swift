d85da7358e91c3ff1bb542265641083261ae5534
Brianna Tanusi <btanusi@gmail.com>
2019-11-23 20:19:37 -0800

    fixed ingredientVC background

--- Frydge/Frydge/src/IngredientViewController.swift
+++ Frydge/Frydge/src/IngredientViewController.swift
@@ -21,58 +21,45 @@ class IngredientViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let header: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Sample Text"
+        label.font = UIFont(name: "Comfortaa", size: 42)
+        label.textColor = .black
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .black
+
         let backgroundImage = UIImageView(image: #imageLiteral(resourceName: "marble"))
-        backgroundImage.frame = view.frame
-        backgroundImage.contentMode = .scaleAspectFill
-        backgroundImage.alpha = 0.5
-        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
-        let fadeView = UIView(frame: backgroundImage.frame)
-        fadeView.backgroundColor = .white
-        fadeView.alpha = 0.5
-        backgroundImage.addSubview(fadeView)
+            backgroundImage.contentMode = .scaleAspectFill
+            backgroundImage.alpha = 0.5
+            backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+
+        view.backgroundColor = UIColor(white: 1, alpha: 1)
+        
         view.addSubview(backgroundImage)
+
         var list = [
             backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundImage.leftAnchor.constraint(equalTo: view.leftAnchor),
             backgroundImage.rightAnchor.constraint(equalTo: view.rightAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ]
-        
-        guard let ingredient = ingredient else { return }
 
+        NSLayoutConstraint.activate(list)
         
-        titleView = createTitleView()
-        titleView?.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(titleView!)
-        NSLayoutConstraint.activate([
-            titleView!.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            titleView!.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
-            titleView!.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
-            titleView!.heightAnchor.constraint(equalToConstant: 100)
-        ])
+        view.addSubview(header)
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 32.0).isActive = true
+        header.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 64.0).isActive = true
     }
     
-    func createTitleView() -> UIView {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.alpha = 0.8
-        
-        let label = UILabel()
-        label.text = ingredient?.name
-        label.font = UIFont(name: "Comfortaa", size: 35)
-        label.textColor = .black
-        label.numberOfLines = 2
-        label.adjustsFontSizeToFitWidth = true
-        label.textAlignment = .left
-        
-        view.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.constrainToContainer(container: view, padding: 10)
-        return view
-    }
+    
 }
+    
+
 
